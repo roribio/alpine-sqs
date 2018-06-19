8cc70201082eb6dd482d4a20145e44bca8e79a09
Michał Sitko <pierwszy1@gmail.com>
2018-06-19 16:04:20 +0200

    modify sqs-init.sh to allow providing custom configuration

--- opt/sqs-init.sh
+++ opt/sqs-init.sh
@@ -1,7 +1,13 @@
 #!/bin/sh 
 
 mkdir -p /opt/config
+
+# First, copy default configs:
 cp /opt/*.conf /opt/config/
+
+# Secondly, copy custom configs:
+cp /opt/custom/*.conf /opt/config/
+
 sleep 1
 exit 0
 
