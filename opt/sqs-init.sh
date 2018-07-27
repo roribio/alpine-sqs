c606944bf7567eef2b46681da1d8db9ace411f29
Janosch Maier <maierjanosch@gmail.com>
2018-07-27 03:11:00 +0200

    Copy sqs-insight config file to correct location (#16)

--- opt/sqs-init.sh
+++ opt/sqs-init.sh
@@ -8,6 +8,9 @@ cp /opt/*.conf /opt/config/
 # Secondly, copy custom configs:
 cp /opt/custom/*.conf /opt/config/
 
+# Now copy sqs-insight config to correct location:
+cp /opt/config/sqs-insight.conf /opt/sqs-insight/config/config_local.json
+
 sleep 1
 exit 0
 
