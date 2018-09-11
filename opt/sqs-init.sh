54398074b447910790235b74d7c67f368dace1c7
Neil <neils@knowbe4.com>
2018-09-11 16:05:07 -0400

    now configurable with env vars

--- opt/sqs-init.sh
+++ opt/sqs-init.sh
@@ -1,20 +1,36 @@
-#!/bin/bash 
+#!/bin/bash
 
 if [ -z "$VISIBLE_QUEUES" ]; then
     echo "No queues set, using default"
     export VISIBLE_QUEUES=default
 fi
 
-arr=$(echo $VISIBLE_QUEUES | tr "," "\n")
-pos=$(( ${#arr[*]} - 1 ))
-last=${arr[$pos]}
+if [ -z "$QUEUE_ACCESS_KEY" ]; then
+    echo "Access key not set, using default"
+    export QUEUE_ACCESS_KEY=notValidKey
+fi
 
-echo -n "{\"port\":9325,\"rememberMessages\":100,\"endpoints\":[" > /opt/sqs-insight.conf
+if [ -z "$QUEUE_SECRET_KEY" ]; then
+    echo "Secret key not set, using default"
+    export QUEUE_SECRET_KEY=notValidSecret
+fi
+
+arr=$(echo $VISIBLE_QUEUES | tr "," "\n")
+
+INSIGHT=$(echo -n "{\"port\":9325,\"rememberMessages\":100,\"endpoints\":[")
+ELASTICMQ=$(echo -e "include classpath(\"application.conf\")\n\nnode-address {\n\tprotocol = http\n\thost = \"*\"\n\tport = 9324\n\tcontext-path = \"\"\n}\n\nrest-sqs {\n\tenabled = true\n\tbind-port = 9324\n\tbind-hostname = \"0.0.0.0\"\n\tsqs-limits = strict\n}\n\nqueues {")
 for x in $arr;
 do
-  echo -n "{\"key\":\"access_key\",\"secretKey\":\"secret_key\",\"region\":\"us-east-1\",\"url\":\"http://localhost:9324/queue/$x\",\"visibility\":0}," >> /opt/sqs-insight.conf
+    INSIGHT+=$(echo -n "{\"key\":\"$QUEUE_ACCESS_KEY\",\"secretKey\":\"$QUEUE_SECRET_KEY\",\"region\":\"us-east-1\",\"url\":\"http://localhost:9324/queue/$x\",\"visibility\":0},")
+    ELASTICMQ+=$(echo -e "\n\t$x {\n\t\tdefaultVisibilityTimeout = 10 seconds\n\t\tdelay = 5 seconds\n\t\treceiveMessageWait = 0 seconds\n\t},\n")
 done
+
+echo "${INSIGHT}" > /opt/sqs-insight.conf
 printf '%s\n' '$' 's/.$/]}/' wq | ex /opt/sqs-insight.conf
+
+echo "${ELASTICMQ}" > /opt/elasticmq.conf
+printf '%s\n' '$' 's/.$//' wq | ex /opt/elasticmq.conf
+echo -e "}\n" >> /opt/elasticmq.conf
 
 mkdir -p /opt/config
 
